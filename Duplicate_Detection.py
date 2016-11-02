85cf4646522b5117701e3b5992bb34511b84f408
root <root@localhost.localdomain>
2016-11-02 17:28:50 -0400

    add item to list

--- Duplicate_Detection.py
+++ Duplicate_Detection.py
@@ -2,10 +2,10 @@ from simserver import SessionServer
 from gensim import utils
 import logging
 
-service = SessionServer('/tmp/test/')
+service = SessionServer('/tmp/')
 
 #put the content you want to compare in this list. 
-texts=['this is one document','this is the second','this is one document']
+texts=['this is one document','this is one second','this is one document','this is one second']
 
 
 corpus = [{'id': 'doc_%i' % num, 'tokens': utils.simple_preprocess(text)}
@@ -17,8 +17,10 @@ lst=[]
 for ix in corpus:
     score=service.find_similar(ix['id'])
     for item in score:
-        if item[1]>.7:      #Closer to 1 means identical content
+        print item[1]
+	if item[1]>.7:      #Closer to 1 means identical content
             lst.append((ix['id'],item[0],item[1]) if ix['id']!=item[0] else '')
 
 
 print lst
+#print service.find_similar('doc_1')
